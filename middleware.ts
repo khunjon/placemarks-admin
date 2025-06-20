9801157f28d40a04c26c56a0491cdf813091ce83
khunjon <jonathan.thongtan@gmail.com>
2025-06-20 18:05:26 +0700

    chore: simplify middleware for debugging

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
-import { updateSession } from '@/lib/supabase/middleware'
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
 }
 
 export const config = {
@@ -12,7 +11,6 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
      */
     '/((?!_next/static|_next/image|favicon.ico).*)',
   ],
