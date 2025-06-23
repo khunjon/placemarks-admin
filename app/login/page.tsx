fe58eb6c72dfb779c0b12f7170acb5d24724ddec
khunjon <jonathan.thongtan@gmail.com>
2025-06-23 14:02:05 +0700

    fix build issue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
         router.push('/')
         router.refresh()
       }
-    } catch (err) {
+    } catch {
       setError('AUTHENTICATION ERROR')
     }
     
